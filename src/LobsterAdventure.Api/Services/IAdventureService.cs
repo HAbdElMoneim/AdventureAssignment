ece4734d66edd0cf51494e7141e670c8304aecd1
HAbdElMoneim <HAbdElMoneim2022@gmail.com>
2022-10-01 04:50:23 +0200

    Add Solution Files for Adventure Assignment

---
+++ src/LobsterAdventure.Api/Services/IAdventureService.cs
@@ -0,0 +1,10 @@
+﻿using LobsterAdventure.Core.Models;
+
+namespace LobsterAdventure.Api.Services
+{
+    public interface IAdventureService
+    {
+        Task<bool> AddNewAdventure(string?[] adventureTreeArray, CancellationToken cancellationToken);
+        Task<AdventureTreeNode?> GetAdventure(CancellationToken cancellationToken);
+    }
+}
